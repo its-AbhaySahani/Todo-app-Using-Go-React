013a48192e9997b40a9e08398e814b1f38d04231
Abhay Sahani <abhay.12115826@lpu.in>
2025-02-07 12:51:31 +0530

    initialize react and go

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import {Conatainer} from "sementic-ui-react";
+import ToDoList from "./To-do-lists";
+
+function App(){
+  return(
+    <div>
+      <container>
+        <ToDoList/>
+      </container>
+    </div>
+  )
+}
---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/its-AbhaySahani/Todo-app-Using-Go-React
+
+go 1.23.6
