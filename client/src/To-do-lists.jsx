8d5063ffb8d79e8c367344ad8928cd9b0fadd906
Abhay Sahani <abhay.12115826@lpu.in>
2025-02-07 13:20:39 +0530

    made todoLIST.JSX

---
+++ client/src/To-do-lists.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from "React";
+import axios from "axios";
+import {Card, Header, Form, Input, Icon} from "semantic-ui-react";
+
+let endpoint = "http://localhost:8000";
+
+class ToDoList extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state={
+            task:"",
+            items:[],
+        }
+    }
+    ComponentDidMount(){
+        this.getTask();
+    }
+
+    render(){
+        return(
+            <div>
+                <div className = "row">
+                    <Header className ="header" as="h2" color="yellow">
+                        To Do List
+                    </Header>
+                </div>
+                <div className="row">
+                    <Form onSubmit={this.onSubmit}>
+                        <Input
+                        type="text"
+                        name="task"
+                        onChange={this.onChange}
+                        value={this.state.task}
+                        fluid
+                        placeholder="Create Task"
+                        />
+                    </Form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ToDoList;
