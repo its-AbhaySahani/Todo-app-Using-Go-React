a31d43fcef9baff7506c496ba9871125e6564e56
Abhay Sahani <abhay.12115826@lpu.in>
2025-02-21 18:11:04 +0530

    driver.go

--- server/persistent/shared_todos_repository/driver.go
+++ server/persistent/shared_todos_repository/driver.go
@@ -3,15 +3,10 @@ package shared_todos_repository
 import (
 	"database/sql"
 
-	"github.com/its-AbhaySahani/Todo-app-Using-Go-React/domain"
 	"github.com/its-AbhaySahani/Todo-app-Using-Go-React/models/db"
 )
 
-func NewSharedTodoQueries(DB *sql.DB) *db.Queries {
-	return db.New(DB)
-}
-
-func NewSharedTodoRepository(DB *sql.DB) domain.SharedTodoRepository {
-	querier := NewSharedTodoQueries(DB)
+func NewSharedTodoRepository(DB *sql.DB) *SharedTodoRepository {
+	querier := db.New(DB)
 	return &SharedTodoRepository{querier: querier}
 }
